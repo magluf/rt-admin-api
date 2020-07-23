66f49a6a6ea2c2207fee88b7ac9d2ce70303a6bb
Sergio Magluf <smagluf@gmail.com>
2020-07-23 20:30:09 -0300

    Port change for GCP, maybe?

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,6 @@ app.get('/', function (req, res) {
   res.send(`Hello ${user.name}`);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
