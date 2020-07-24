ae80db1a0020d397deee35dabd6718829e318cdd
Sergio Magluf <smagluf@gmail.com>
2020-07-23 22:31:58 -0300

    More Heroku tests.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-// lib/app.ts
 import express = require('express');
+import path = require('path');
 import { User } from './model/user.model';
 
+const PORT = process.env.PORT || 5000;
 const app: express.Application = express();
 const user = new User(1, 'Milena');
 
-app.get('/', function (req, res) {
-  res.send(`Hello ${user.name}`);
-});
-
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.send(`Hello ${user.name}`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
